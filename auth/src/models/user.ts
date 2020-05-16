127a7413de714a37d253f480184ae098ebb9f32c
hsimao <e087754958@gmail.com>
2020-05-16 16:26:11 +0800

    feat: mongo user model 回傳參數 ts 驗證

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,12 +1,20 @@
 import mongoose from 'mongoose'
 
+// 新增用戶的請求參數
 interface UserAttrs {
   email: string
   password: string
 }
 
-interface UserModel extends mongoose.Model<any> {
-  build(attrs: UserAttrs): any
+// user model 新增 build 方式, 並關聯 UserAttrs interface
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc
+}
+
+// 創建 user 成功後需回傳的資料格式
+interface UserDoc extends mongoose.Document {
+  email: string
+  password: string
 }
 
 const userSchema = new mongoose.Schema({
@@ -26,17 +34,22 @@ userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs)
 }
 
-const User = mongoose.model<any, UserModel>('User', userSchema)
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema)
 
-const buildUser = (attrs: UserAttrs) => {
-  return new User(attrs)
-}
-
-// 測試 typescript 效驗
+// 測試 input 參數效驗
 // User.build({
 //   email: '3040',
 //   password: '122',
 //   age: 30, // 此屬性 UserAttrs 沒有, typtscript 會警告
 // })
+
+// 測試 return 參數效驗
+// const user = User.build({
+//   email: 'test@gmail.com',
+//   password: '12344',
+// })
+// user.email
+// user.password
+// user.updatedAt // 此屬性沒有定義, typescript 會警告
 
 export { User }
