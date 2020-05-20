df343b7666457bf1470fecd2c222fe5840276980
hsimao <e087754958@gmail.com>
2020-05-20 20:03:35 +0800

    test: current-user

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,15 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
+import request from 'supertest'
 import { app } from '../app'
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>
+    }
+  }
+}
 
 let mongo: any
 
@@ -29,3 +38,21 @@ afterAll(async () => {
   await mongo.stop()
   await mongoose.connection.close()
 })
+
+// 封裝常用註冊邏輯到 global
+global.signin = async () => {
+  const email = 'test@test.com'
+  const password = 'password'
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201)
+
+  const cookie = response.get('Set-Cookie')
+
+  return cookie
+}
