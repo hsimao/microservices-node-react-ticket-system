bbfc21f774713c578722c53c1b1a079f43e72e86
hsimao <e087754958@gmail.com>
2020-05-21 20:59:12 +0800

    feat: 設定讓 next server side 發出的 api 可訪問到內部服務 api

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,29 @@
-export default () => {
+import axios from 'axios'
+
+const LandingPage = ({ currentUser }) => {
+  console.log('I am in the componet', currentUser)
+  // axios.get('/api/users/currentuser')
   return <h1>Landing Page</h1>
 }
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    // requests should be made to http://SERVICENAME.NAMESPACE.svc.cluster.local
+    const ingressNginxUrl = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
+    const { data } = await axios.get(`${ingressNginxUrl}/api/users/currentuser`, {
+      headers: {
+        Host: 'ticketing.dev', // ingress-srv 配置的 host
+      },
+    })
+    return data
+  } else {
+    // we are on the browser!
+    // requests can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser')
+    return data
+  }
+  return {}
+}
+
+export default LandingPage
