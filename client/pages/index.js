be37eab7b954577d965f711fed49d9c961075411
hsimao <e087754958@gmail.com>
2020-05-23 16:39:23 +0800

    feat: 封裝 axios

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,29 +1,15 @@
-import axios from 'axios'
+import buildClient from '../api/build-client'
 
 const LandingPage = ({ currentUser }) => {
   console.log('I am in the componet', currentUser)
-  // axios.get('/api/users/currentuser')
   return <h1>Landing Page</h1>
 }
 
-LandingPage.getInitialProps = async () => {
-  if (typeof window === 'undefined') {
-    // we are on the server!
-    // requests should be made to http://SERVICENAME.NAMESPACE.svc.cluster.local
-    const ingressNginxUrl = 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local'
-    const { data } = await axios.get(`${ingressNginxUrl}/api/users/currentuser`, {
-      headers: {
-        Host: 'ticketing.dev', // ingress-srv 配置的 host
-      },
-    })
-    return data
-  } else {
-    // we are on the browser!
-    // requests can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser')
-    return data
-  }
-  return {}
+LandingPage.getInitialProps = async content => {
+  const client = buildClient(content).get('/api/users/currentuser')
+  const { data } = await client.get('/api/users/currentuser')
+
+  return data
 }
 
 export default LandingPage
