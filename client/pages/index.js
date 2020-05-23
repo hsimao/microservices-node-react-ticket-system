6d135a21a6325bcbef3d899f98a3ccd75ffe52ba
hsimao <e087754958@gmail.com>
2020-05-23 20:31:35 +0800

    feat: header、signout

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,10 @@
 import buildClient from '../api/build-client'
 
 const LandingPage = ({ currentUser }) => {
-  console.log('I am in the componet', currentUser)
   return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
 }
 
 LandingPage.getInitialProps = async content => {
-  console.log('Landing page!')
   const client = buildClient(content)
   const { data } = await client.get('/api/users/currentuser')
 
