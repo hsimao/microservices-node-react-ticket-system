0ee014ae08deb75fdb18dec98e43a0731c4273db
hsimao <e087754958@gmail.com>
2020-05-27 20:31:05 +0800

    feat: get tickets logic and test

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import cookieSession from 'cookie-session'
 import { errorHandler, NotFoundError, currentUser } from '@hsimao-tickets/common'
 import { createTicketRouter } from './routes/new'
 import { showTicketRouter } from './routes/show'
+import { indexTicketRouter } from './routes/index'
 
 const app = express()
 app.set('trust proxy', true)
@@ -18,9 +19,10 @@ app.use(
 )
 
 app.use(currentUser)
-app.use(showTicketRouter)
 
 app.use(createTicketRouter)
+app.use(showTicketRouter)
+app.use(indexTicketRouter)
 
 app.all('*', async (req, res) => {
   throw new NotFoundError()
