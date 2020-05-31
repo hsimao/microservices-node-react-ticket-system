1d0330727233930cf11d67082cbd3ce4a657fab0
hsimao <e087754958@gmail.com>
2020-05-31 16:41:17 +0800

    feat: create ticket 使用 nats 發送 Publisher

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express'
 import { body } from 'express-validator'
 import { requireAuth, validateRequest } from '@hsimao-tickets/common'
 import { Ticket } from '../models/ticket'
+import { TicketCreatedPublisher } from '../events/publishers/ticket-created-publisher'
+import { natsWrapper } from '../nats-wrapper'
 
 const router = express.Router()
 
@@ -28,6 +30,14 @@ router.post(
     })
 
     await ticket.save()
+
+    // 不使用用戶直接傳遞的 title、price, 而使用 mongo 返回的 ticket.title ... 可確保資料是經過 mongoose 過濾過且真正儲存到資料庫的資料
+    new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    })
 
     res.status(201).send(ticket)
   }
