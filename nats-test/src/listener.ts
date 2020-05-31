49ccbf9242f7352c414e26b688779102d0c61e82
hsimao <e087754958@gmail.com>
2020-05-31 14:13:06 +0800

    use: featicketCreatedListener class

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -15,25 +15,7 @@ stan.on('connect', () => {
     process.exit()
   })
 
-  const options = stan
-    .subscriptionOptions()
-    // 手動判斷是否接收成功，若 30 秒內沒有成功將會自動找另外的 queue 發送, 直到成功
-    .setManualAckMode(true)
-    .setDeliverAllAvailable() // 每次重啟 listen port 都會重新執行之前執行過的所有 subscrip 任務
-    .setDurableName('accounting-service') // 將會記錄訂閱狀態，有發出去成功後會標記，將在下次重啟時不用在重新發送
-
-  const subscription = stan.subscribe('ticket:created', 'queue-group-name', options)
-
-  subscription.on('message', (msg: Message) => {
-    const data = msg.getData()
-
-    if (typeof data === 'string') {
-      console.log('data', data)
-      console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
-    }
-
-    msg.ack() // 確認接收成功
-  })
+  new TicketCreatedListener(stan).listen()
 })
 
 process.on('SIGINT', () => stan.close())
@@ -53,10 +35,10 @@ abstract class Listener {
   subscriptionOptions() {
     return this.client
       .subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setAckWait(this.ackWait)
-      .setDurableName(this.queueGroupName)
+      .setDeliverAllAvailable() // 每次重啟 listen port 都會重新執行之前執行過的所有 subscrip 任務
+      .setManualAckMode(true) // 手動判斷是否接收成功，若失敗將會在 30秒(預設) 後自動找另外的 queue 發送, 直到成功
+      .setAckWait(this.ackWait) // 若失敗後幾秒後要重新發送 (預設30秒)
+      .setDurableName(this.queueGroupName) // 將會記錄訂閱狀態，有發出去成功後會標記，將在下次重啟時不用在重新發送
   }
 
   listen() {
