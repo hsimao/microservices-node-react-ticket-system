3e43f316a4953ed7329d292ad0795a7ea6ab0c5b
hsimao <e087754958@gmail.com>
2020-06-01 21:31:45 +0800

    feat: order models

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,24 @@
+import mongoose from 'mongoose'
 import express, { Request, Response } from 'express'
+import { requireAuth, validateRequest } from '@hsimao-tickets/common'
+import { body } from 'express-validator'
 
 const router = express.Router()
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({})
-})
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input)) // 驗證 ticketId 是否符合 mongo id 格式
+      .withMessage('TicketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({})
+  }
+)
 
 export { router as createOrderRouter }
