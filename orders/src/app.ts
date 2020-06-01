31ce2179b4c9569189bf3707440d3f27a2190870
hsimao <e087754958@gmail.com>
2020-06-01 20:56:12 +0800

    feat: orders base route api

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -4,6 +4,11 @@ import { json } from 'body-parser'
 import cookieSession from 'cookie-session'
 import { errorHandler, NotFoundError, currentUser } from '@hsimao-tickets/common'
 
+import { indexOrderRouter } from './routes/index'
+import { showOrderRouter } from './routes/show'
+import { createOrderRouter } from './routes/new'
+import { deleteOrderRouter } from './routes/delete'
+
 const app = express()
 app.set('trust proxy', true)
 
@@ -16,6 +21,11 @@ app.use(
 )
 
 app.use(currentUser)
+
+app.use(indexOrderRouter)
+app.use(showOrderRouter)
+app.use(createOrderRouter)
+app.use(deleteOrderRouter)
 
 app.all('*', async (req, res) => {
   throw new NotFoundError()
