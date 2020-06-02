0ba95da72630f8a257add176e3fe312ed42c934a
hsimao <e087754958@gmail.com>
2020-06-02 20:12:34 +0800

    feat: create order api test

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,54 @@
+import request from 'supertest'
+import { app } from '../../app'
+import { Order, OrderStatus } from '../../models/order'
+import { Ticket } from '../../models/ticket'
+
+it('returns an error if the ticket does not exist', async () => {
+  const ticketId = await global.createMongoId()
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({
+      ticketId,
+    })
+    .expect(404)
+})
+
+it('returns an error if the ticket is already reserved', async () => {
+  // 先創建一個票券與訂單
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  })
+  await ticket.save()
+
+  const order = Order.build({
+    ticket,
+    userId: 'sdfisjh',
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
+  })
+  await order.save()
+
+  // 再新增一筆有相同票券的訂單，預期回應 400
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(400)
+})
+
+it('reserves a ticket', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  })
+  await ticket.save()
+
+  await request(app)
+    .post('/api/orders')
+    .set('Cookie', global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201)
+})
---
+++ orders/src/test/setup.ts
@@ -0,0 +1,71 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): string[]
+      createMongoId(): string
+    }
+  }
+}
+
+jest.mock('../nats-wrapper')
+
+let mongo: any
+
+beforeAll(async () => {
+  process.env.JWT_KEY = 'test'
+
+  mongo = new MongoMemoryServer()
+  const mongoUri = await mongo.getUri()
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+})
+
+// 進行每次測試前, 刪除所有 mongo 與 mock 資料
+beforeEach(async () => {
+  jest.clearAllMocks()
+  const collections = await mongoose.connection.db.collections()
+
+  for (let collection of collections) {
+    await collection.deleteMany({})
+  }
+})
+
+afterAll(async () => {
+  await mongo.stop()
+  await mongoose.connection.close()
+})
+
+// 封裝常用註冊邏輯到 global
+global.signin = () => {
+  // Build a JWT payload. { id, email }
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@gmail.com',
+  }
+
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!)
+
+  // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token }
+
+  // Trun that session into JSON
+  const sessionJSON = JSON.stringify(session)
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64')
+
+  // return a string thats the cookie with the encode data
+  return [`express:sess=${base64}`]
+}
+
+global.createMongoId = () => {
+  return new mongoose.Types.ObjectId().toHexString()
+}
