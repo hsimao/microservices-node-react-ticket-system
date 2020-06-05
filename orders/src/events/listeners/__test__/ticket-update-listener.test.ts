1d9b0f9502ad1f78a1b1c00dfd27ec220250201d
hsimao <e087754958@gmail.com>
2020-06-05 14:25:28 +0800

    feat: order ticket-update-listener 新增 version 版本號錯誤測試

--- orders/src/events/listeners/__test__/ticket-update-listener.test.ts
+++ orders/src/events/listeners/__test__/ticket-update-listener.test.ts
@@ -54,3 +54,15 @@ it('acks the message', async () => {
 
   expect(msg.ack).toHaveBeenCalled()
 })
+
+// 當 version 號碼不是下一個，則不能呼叫 ack
+it('does not call ack if the event has a skipped version number', async () => {
+  const { msg, data, listener } = await setup()
+
+  data.version = 10
+  try {
+    await listener.onMessage(data, msg)
+  } catch (err) {}
+
+  expect(msg.ack).not.toHaveBeenCalled()
+})
