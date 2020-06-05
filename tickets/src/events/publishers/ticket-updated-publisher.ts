3be5a8c8287fc7d0bd93a64e8aacc8a7debc0b7e
hsimao <e087754958@gmail.com>
2020-06-05 19:35:56 +0800

    feat: ticket order-created-listener 新增 update publisher logic and test

--- tickets/src/events/publishers/ticket-updated-publisher.ts
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -1,5 +1,5 @@
 import { Publisher, Subjects, TicketUpdatedEvent } from '@hsimao-tickets/common'
 
 export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
-  subject: Subjects.TicketUpdated= Subjects.TicketUpdated
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated
 }
