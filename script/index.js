a670b8523b54c0a157464cd1790139bf970752b6
hsimao <e087754958@gmail.com>
2020-06-05 13:03:10 +0800

    feat: ticket、order 新增 version 型別註解, ticket update listener 新增 version 判斷

--- script/index.js
+++ script/index.js
@@ -1,8 +1,8 @@
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-const axios = require('axios');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
+const axios = require('axios')
 
 const cookie =
-  'express:sess=eyJqd3QiOiJleUpoYkdjaU9pSklVekkxTmlJc0luUjVjQ0k2SWtwWFZDSjkuZXlKcFpDSTZJalZsT0dZME5qQTFOak5oWVRkbE1EQXlZMkl3TUdSbU5pSXNJbVZ0WVdsc0lqb2lkR1Z6ZERFd1FIUmxjM1F1WTI5dElpd2lhV0YwSWpveE5UZzJOVE0yTlRBMWZRLlh3R3p5UVZHYnFSaHZ1YWJDMTdsYzNtYlpQNU1XMnl1UU1kODU1Y0hEM3MifQ==';
+  'express:sess=eyJqd3QiOiJleUpoYkdjaU9pSklVekkxTmlJc0luUjVjQ0k2SWtwWFZDSjkuZXlKcFpDSTZJalZsT0dZME5qQTFOak5oWVRkbE1EQXlZMkl3TUdSbU5pSXNJbVZ0WVdsc0lqb2lkR1Z6ZERFd1FIUmxjM1F1WTI5dElpd2lhV0YwSWpveE5UZzJOVE0yTlRBMWZRLlh3R3p5UVZHYnFSaHZ1YWJDMTdsYzNtYlpQNU1XMnl1UU1kODU1Y0hEM3MifQ=='
 
 const doRequest = async () => {
   const { data } = await axios.post(
@@ -11,7 +11,7 @@ const doRequest = async () => {
     {
       headers: { cookie },
     }
-  );
+  )
 
   await axios.put(
     `https://ticketing.dev/api/tickets/${data.id}`,
@@ -19,7 +19,7 @@ const doRequest = async () => {
     {
       headers: { cookie },
     }
-  );
+  )
 
   axios.put(
     `https://ticketing.dev/api/tickets/${data.id}`,
@@ -27,13 +27,13 @@ const doRequest = async () => {
     {
       headers: { cookie },
     }
-  );
+  )
 
-  console.log('Request complete');
-};
+  console.log('Request complete')
+}
 
-(async () => {
-  for (let i = 0; i < 200; i++) {
-    doRequest();
+;(async () => {
+  for (let i = 0; i < 400; i++) {
+    doRequest()
   }
-})();
+})()
