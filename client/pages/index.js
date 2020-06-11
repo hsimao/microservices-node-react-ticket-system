bdc6fe85c7f4053ed0f2a962934aca1f6e1ea4fa
hsimao <e087754958@gmail.com>
2020-06-11 20:12:04 +0800

    fix: 修復回首頁 call twice api

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,9 @@
-import buildClient from '../api/build-client'
-
 const LandingPage = ({ currentUser }) => {
   return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
 }
 
 LandingPage.getInitialProps = async content => {
-  const client = buildClient(content)
-  const { data } = await client.get('/api/users/currentuser')
-
-  return data
+  return {}
 }
 
 export default LandingPage
