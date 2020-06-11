1f75f5e21977208389ce9ed26a266780c9a317c0
hsimao <e087754958@gmail.com>
2020-06-11 20:15:33 +0800

    feat: 將封裝好的 api 實例透過 getInitialProps 傳遞

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,7 +2,7 @@ const LandingPage = ({ currentUser }) => {
   return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>
 }
 
-LandingPage.getInitialProps = async content => {
+LandingPage.getInitialProps = async (content, client, currentUser) => {
   return {}
 }
 
